74c9bc2bc6561e85c4c31dfbf15eb44aa8fb5dea
Brian Wangila <bwanjalah@gmail.com>
2022-10-16 14:30:30 +0300

    create build

---
+++ public/precache-manifest.5b48ad65852f39c00821e2d6cc9fd37c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "872d5094ebb503bca73e",
+    "url": "/static/css/main.59b525c7.chunk.css"
+  },
+  {
+    "revision": "872d5094ebb503bca73e",
+    "url": "/static/js/main.0662bdbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ac264a8d62dfe756664",
+    "url": "/static/js/2.aa210b43.chunk.js"
+  },
+  {
+    "revision": "212741ecc92291ab6192ae1fea3423d6",
+    "url": "/index.html"
+  }
+];
